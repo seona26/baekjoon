de692bbf943f8ccdb0716c5167d4b8fd3c32cedd
seona26 <58184617+seona26@users.noreply.github.com>
2023-08-04 18:27:00 +0900

    [Silver V] Title: 방 번호, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1475. 방 번호/방 번호.cc
+++ 백준/Silver/1475. 방 번호/방 번호.cc
@@ -4,15 +4,15 @@ using namespace std;
 int main(){
     ios::sync_with_stdio(0);
     cin.tie(0);
-    int N, max = 0;
+    int N, Max = 0;
     int num[10] = {};
     cin >> N;
     while(N!=0){
         num[N%10]++;
         N/=10;
     }
-    num[6] = (num[6]+num[9])/2+(num[6]+num[9])%2;
+    num[6] = (num[6]+num[9]+1)/2;
     num[9] = 0;
-    for(int n:num)  if(n>max)   max = n;
-    cout << max;
+    for(int n:num)  if(n>Max)   Max = n;
+    cout << Max;
 }
