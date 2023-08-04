af65a72b78e445ccae0766100a9f73444e3d36d5
seona26 <58184617+seona26@users.noreply.github.com>
2023-08-04 17:58:15 +0900

    [Bronze IV] Title: 알파벳 개수, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/10808. 알파벳 개수/알파벳 개수.cc
@@ -0,0 +1,12 @@
+#include <bits/stdtr1c++.h>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    vector<int> vex(26);
+    string s;
+    cin >> s;
+    for(int i = 0; i<s.size(); i++) vex[s[i]-0x61]++;
+    for(int n:vex)  cout << n << " ";
+}
