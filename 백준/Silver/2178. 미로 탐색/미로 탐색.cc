6861f567d5df31aa2fc30303f55bc620c5af8e54
seona26 <58184617+seona26@users.noreply.github.com>
2023-11-09 19:50:59 +0900

    [Silver I] Title: 미로 탐색, Time: 0 ms, Memory: 2200 KB -BaekjoonHub

---
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.cc
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+queue<int> que;
+int n, m, cnt[100][100];
+string maze[100];
+int row[4] = {-1, 1, 0, 0};
+int col[4] = {0, 0, -1, 1};
+
+void BFS(){
+    if(que.front()==n*m-1 || que.empty()) return;
+    
+    int r = que.front()/m;
+    int c = que.front()%m;
+    //cout << r << c << endl;
+    int a, b;
+    que.pop();
+
+    for(int i = 0; i<4; i++){
+        a = r+row[i];
+        b = c+col[i];
+        //cout << a << b << endl;
+        if(a<0 || a>=n) continue;
+        if(b<0 || b>=m) continue;
+        
+            if(maze[a][b] == '1'){
+                cnt[a][b] = cnt[r][c]+1;
+                que.push(a*m + b);
+                maze[a][b] = '0';
+                //cout << a << b << endl;
+            }
+            // else{
+            //     if(maze[a][b]>maze[r][c]+1){
+            //         maze[a][b] = maze[r][c]+1;
+            //         que.push(a*m + b);
+            //     }
+            // }
+            //cout << a << b << endl;
+    }
+    BFS();
+    return;
+}
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> n >> m;
+    for(int i = 0; i<n; i++)
+        cin >> maze[i];
+
+    que.push(0);
+    cnt[0][0] = 1;
+    BFS();
+
+    cout << cnt[n-1][m-1];
+}
