173d7beebad406b7e54f0f66e0a1eb4175cba3f6
seona26 <58184617+seona26@users.noreply.github.com>
2023-11-10 09:03:36 +0900

    [Silver I] Title: 곱셈, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1629. 곱셈/곱셈.cc
+++ 백준/Silver/1629. 곱셈/곱셈.cc
@@ -1,23 +1,20 @@
- // Authored by : BaaaaaaaaaaarkingDog
-// Co-authored by : -
-// http://boj.kr/a9f89b45ac624c2a8d13f27a01dd78d1
 #include <bits/stdc++.h>
 using namespace std;
 
-using ll = long long;
+long long Mul(int a, int b, int c){
+    long long val;
+    if(b==1)    return a%c;
 
-ll POW(ll a, ll b, ll m){
-  if(b==1) return a % m;
-  ll val = POW(a, b/2, m);
-  val = val * val % m;
-  if(b%2 == 0) return val;
-  return val * a % m;
+    val = Mul(a, b/2, c);
+    val = val*val%c;
+    if(b%2==0)  return val;
+    return val*a % c;
 }
-
-int main(void){
-  ios::sync_with_stdio(0);
-  cin.tie(0);
-  ll a,b,c;
-  cin >> a >> b >> c;
-  cout << POW(a,b,c);
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    int a, b, c;
+    cin >> a >> b >> c;
+    cout << Mul(a, b, c);
 }
